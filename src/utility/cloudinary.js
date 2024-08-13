f274ecbda12ada5480815a390710266b36eb7386
assunnahmuslimit <abdullah.limonbau@gmail.com>
2024-08-13 10:34:27 +0600

    Increase aximum time duration 2

--- src/utility/cloudinary.js
+++ src/utility/cloudinary.js
@@ -3,6 +3,7 @@ const cloudinary = require("cloudinary").v2;
 const dotenv = require("dotenv");
 dotenv.config({ path: "../.env" });
 
+//cloudinary config file
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
