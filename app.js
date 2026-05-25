685b1c6da2a7e435ca1906d0b285e93e21925383
Abdullah Al Amin <49281872+bigosofts@users.noreply.github.com>
2026-05-25 23:38:37 +0600

    Delete duplicate mongoose call

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Mongo DB Database Connection
-const mongoose = require("mongoose");
 
 const URI = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@internetmadrasha.oo78neo.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
 
